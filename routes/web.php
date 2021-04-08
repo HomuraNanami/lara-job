5734a11e1d8701411ad6799e802cafad2025b312
homura.nanami <psychopath1919@gmail.com>
2021-04-08 00:52:51 +0000

    マルチログイン機能つきlaravelをコピー

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+// ユーザー
+Route::namespace('User')->prefix('user')->name('user.')->group(function () {
+
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'reset'    => false,
+        'verify'   => true
+    ]);
+
+    // ログイン認証後
+    Route::group(['middleware' => ['auth:user', 'verified']], function () {
+
+        // TOPページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+
+    });
+});
+
+// 管理者
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'reset'    => false,
+        'verify'   => false
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:admin')->group(function () {
+
+        // TOPページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+
+    });
+
+});
