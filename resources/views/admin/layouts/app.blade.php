c27272806b88e71d86988575a0dbcec8eef6d4cc
homura.nanami <psychopath1919@gmail.com>
2021-04-11 07:12:25 +0000

    管理画面layoutのダッシュボードへのリンク修正

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -47,7 +47,7 @@
 	  <!-- Main Sidebar Container -->
 	  <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	    <!-- Brand Logo -->
-	    <a href="top.html" class="brand-link">
+	    <a href="{{ route('admin.home') }}" class="brand-link">
 	      <img src="{{ asset('/assets/admin/images/site-icon.jpg') }}" alt="{{ config('app.name', 'Laravel') }} Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 	      <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel') }}</span>
 	    </a>
@@ -70,7 +70,7 @@
 	          <!-- Add icons to the links using the .nav-icon class
 	               with font-awesome or any other icon font library -->
             <li class="nav-item">
-              <a href="top.html" class="nav-link {{ request()->route()->named('admin.home') ? 'active' : '' }}">
+              <a href="{{ route('admin.home') }}" class="nav-link {{ request()->route()->named('admin.home') ? 'active' : '' }}">
                 <i class="fas fa-tachometer-alt nav-icon"></i>
                 <p>
                   ダッシュボード
